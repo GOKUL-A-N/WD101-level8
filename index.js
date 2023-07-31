2b2333bc9c523e91db861ccf11a9dc1e7660c2a0
GOKUL-A-N <gokulcricketer141103@gmail.com>
2023-07-31 12:43:54 +0530

    third commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ document.getElementById("form").addEventListener('submit', function(event){
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     const dob = document.getElementById('dob').value;
-    const term = document.getElementById('term').value;
+    const Acceptedterm = document.getElementById('Accepted term').value;
 
     const ans = document.querySelector('#tablebody');
     const row = document.createElement('tr');
@@ -14,7 +14,7 @@ document.getElementById("form").addEventListener('submit', function(event){
     <td>${email}</td>
     <td>${password}</td>
     <td>${dob}</td>
-    <td>${term ? "yes" : "no"}</td>
+    <td>${Acceptedterm ? "yes" : "no"}</td>
     `;
     ans.appendChild(row);
     document.getElementById("form").reset();
